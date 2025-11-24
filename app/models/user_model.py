d04af56b1a9f420955251670f59b2c3af53cb8d3
Vin <vincentwei@healife.com>
2025-11-24 09:59:08 +0800

    add something

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -3,7 +3,8 @@ from sqlalchemy import (
     Integer,
     DateTime,
     String,
-    Boolean
+    Boolean,
+    ForeignKey
 )
 from sqlalchemy.orm import validates
 from .base_model import BaseModel
@@ -11,6 +12,7 @@ from app.database.db import Base
 import re
 
 class User(Base, BaseModel):
+    '''用户表'''
     __tablename__ = 'user'
     
     id = Column(Integer, primary_key=True, autoincrement=True)      # 主键
@@ -19,10 +21,10 @@ class User(Base, BaseModel):
     name = Column(String(255), nullable=True)
     password = Column(String(255), nullable=True)
     avatar = Column(String(255), nullable=True)
-    source = Column(String(255), nullable=True)
-    platform = Column(String(255), nullable=True)
+    source = Column(String(255), nullable=True, comment='注册来源web/app/h5')
+    platform = Column(String(255), nullable=True, comment='平台web/app/h5')
     salt = Column(String(255), nullable=True)
-    status = Column(Boolean, nullable=False, default=True) #0禁用1正常
+    status = Column(Boolean, nullable=False, default=True, comment='0禁用1正常') 
     last_login = Column(DateTime, nullable=True)
     last_ip = Column(String(255), nullable=True)
     
@@ -73,3 +75,40 @@ class User(Base, BaseModel):
                 continue
             raise ValueError("name contains invalid characters")
         return value
+    
+class UserCertification(Base, BaseModel):
+    '''用户认证信息表'''
+    __tablename__ = 'user_certification'
+    id = Column(Integer, primary_key=True, autoincrement=True)      # 主键
+    name = Column(String(255), nullable=False, comment='姓名')
+    user_id = Column(Integer, nullable=False)
+    
+    id_type = Column(Integer, nullable=False, default='身份证/医师资格证', comment='证件类型')
+    id_card_number = Column(String(255), nullable=False, comment="证件号码")
+    id_card_image_front = Column(String(255), nullable=True, comment="证件正面图片链接")
+    id_card_image_back = Column(String(255), nullable=True, comment="证件背面图片链接")
+
+    proof_files = Column(String(500), nullable=True, comment="证明材料图片链接，逗号分隔")
+    status = Column(Integer, nullable=False, default=1, comment='认证状态0无效1有效')
+    submit_time = Column(DateTime, nullable=True, comment='提交时间')
+    audit_status = Column(Integer, nullable=True, default=0, comment='审核状态0未审核1审核通过2审核不通过')
+    audit_time = Column(DateTime, nullable=True, comment='审核时间')
+    audit_user_id = Column(Integer, nullable=True, comment='审核人ID')
+    audit_user_name = Column(String(255), nullable=True, comment='审核人姓名')
+    audit_remark = Column(String(255), nullable=True, comment='审核备注')
+    audit_source = Column(String(255), nullable=True, comment='认证来源web/app/h5')
+    
+    ext_1 = Column(String(255), nullable=True)
+    ext_2 = Column(String(255), nullable=True)
+    ext_3 = Column(String(255), nullable=True)
+    ext_4 = Column(String(255), nullable=True)
+    ext_5 = Column(String(255), nullable=True) 
+    
+    
+class UserCertificationHistory(UserCertification):
+    '''用户认证历史信息表，继承UserCertification的全部信息'''
+    __tablename__ = 'user_certification_history'
+    id = Column(Integer, primary_key=True, autoincrement=True)      # 主键
+    certification_id = Column(Integer, ForeignKey('user_certification.id'), nullable=False, comment='认证ID')
+    
+    
